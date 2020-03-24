6236b2e7961286c3b7fdc3cb50c1734c8268035b
Popescu-Iulian <popescu.iulian@protonmail.com>
2020-03-24 17:45:13 +0200

    GradesBook class

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const STUDENT_NAME = document.querySelector('.student-name');
 const TABLE_BODY = document.querySelector('.table-body');
+const GRADES_BODY = document.querySelector('.grades-table-body');
 
-class Student {
+class Students {
   constructor(name) {
     this.name = name;
     this.grades = [];
-    this.averageGrade = '';
+    this.averageGrade;
   }
 
   addGrade(grade) {
@@ -25,3 +26,43 @@ class Student {
     }
   }
 }
+
+class GradesBook {
+  constructor() {
+    this.students = [];
+    this.evaluatedStudent;
+  }
+
+  addStudent(str) {
+    let student = new Students(str);
+    this.students.push(student);
+  }
+
+  seeGrades(idx) {
+    let student = this.students[idx];
+    this.evaluatedStudent = student;
+  }
+
+  sortAverageGradeAsc() {
+    this.students.sort((a, b) => a.averageGrade - b.averageGrade);
+  }
+
+  sortAverageGradeDesc() {
+    this.students.sort((a, b) => b.averageGrade - a.averageGrade);
+  }
+
+  addGrade(str) {
+    this.evaluatedStudent.addGrade(str);
+  }
+
+  sortGradeAsc() {
+    this.evaluatedStudent.grades.sort((a, b) => a - b);
+  }
+
+  sortGradeDesc() {
+    this.evaluatedStudent.grades.sort((a, b) => b - a);
+  }
+}
+
+let gradesBook = new GradesBook();
+
